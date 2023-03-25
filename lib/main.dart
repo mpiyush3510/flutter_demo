694a0cfb5da402d05725372626bb5df33e862927
Piyush Makwana <mpiyush3510@gmail.com>
2023-03-25 10:09:08 +0530

    Flutter App Developement

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,16 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hello Piyush!"),
-      ),
-    ),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Flutter Demo"),
+            backgroundColor: Color.fromARGB(255, 228, 138, 138),
+          ),
+          body: Image(
+            image: NetworkImage(
+                "https://mpiyush3510.carrd.co/assets/images/image01.jpg?v=db364dae"),
+          ),
+        )),
   );
 }
